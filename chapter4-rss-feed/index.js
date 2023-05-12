73caba84fa806bebb813bf5a4fcf71d887f6db1b
Matt Jackson <mattrjackson1@gmail.com>
2023-05-12 16:57:58 -0700

    Finished chapter 4.

--- chapter4-rss-feed/index.js
+++ chapter4-rss-feed/index.js
@@ -1,13 +1,41 @@
 import Parser from 'rss-parser';
+import promptModule from 'prompt-sync';
+
 const parser = new Parser();
+const prompt = promptModule({ sigint: true });
+const customItems = [];
+
+const urls = [
+  'https://www.bonappetit.com/feed/recipes-rss-feed/rss',
+  'https://www.specialtyfood.com/rss/featured-articles/category/lunch/',
+  'https://www.reddit.com/r/recipes/.rss'
+];
+
+const aggregate = (responses, feedItems) => {
+  for (let { items } of responses) {
+    for (let { title, link } of items) {
+      if (title.toLowerCase().includes('veg')) {
+        feedItems.push({ title, link });
+      }
+    }
+  }
+  return feedItems;
+};
+
+const print = (feedItems) => {
+  console.clear();
+  const [title, link] = res.split(',');
+  if (![title, link].includes(undefined)) customItems.push({ title, link });
+  console.table(feedItems.concat(customItems));
+  console.log('Last Updated: ', new Date().toUTCString());
+  const res = prompt('Add item: ');
+};
 
 const main = async () => {
-  const url = 'https://www.bonappetit.com/feed/recipes-rss-feed/rss';
-  const { title, items } = await parser.parseURL(url);
-  console.log(title);
-  const results = items.map(({ title, link }) => ({ title, link }));
-  console.clear();
-  console.log('Last updated:', new Date().toUTCString());
-  console.table(results);
+  const feedItems = [];
+  const awaitableRequests = urls.map((url) => parser.parseURL(url));
+  const responses = await Promise.all(awaitableRequests);
+  aggregate(responses, feedItems);
+  print(feedItems);
 };
 setInterval(main, 2000);
